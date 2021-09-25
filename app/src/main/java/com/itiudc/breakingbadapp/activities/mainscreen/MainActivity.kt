01b08897740b516e171a39d71fb037371e256125
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2021-09-24 23:33:46 -0500

    Tablayout, change colors

--- app/src/main/java/com/itiudc/breakingbadapp/activities/mainscreen/MainActivity.kt
+++ app/src/main/java/com/itiudc/breakingbadapp/activities/mainscreen/MainActivity.kt
@@ -3,7 +3,11 @@ package com.itiudc.breakingbadapp.activities.mainscreen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.itiudc.breakingbadapp.R
+import com.itiudc.breakingbadapp.adapters.TabLayoutAdapter
 import com.itiudc.breakingbadapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        val tabLayoutAdapter = TabLayoutAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.viewPager2)
+        viewPager.adapter = tabLayoutAdapter
+
+        val tabs: TabLayout = findViewById(R.id.tabLayout)
+
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getStringArray(R.array.tabLayoutStrings)[position]
+        }.attach()
 
     }
 }
---
+++ app/src/main/java/com/itiudc/breakingbadapp/adapters/TabLayoutAdapter.kt
@@ -0,0 +1,24 @@
+package com.itiudc.breakingbadapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.itiudc.breakingbadapp.fragments.CharactersListFragment
+import com.itiudc.breakingbadapp.fragments.PhraseFragment
+import com.itiudc.breakingbadapp.fragments.SettingsFragment
+
+class TabLayoutAdapter(fm: FragmentActivity): FragmentStateAdapter(fm) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> CharactersListFragment()
+            1 -> PhraseFragment()
+            2 -> SettingsFragment()
+            else -> throw IllegalStateException("Unexpected position $position")
+        }
+    }
+
+}
