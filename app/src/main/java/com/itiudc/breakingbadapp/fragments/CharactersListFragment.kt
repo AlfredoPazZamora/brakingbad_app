f7ee1b43fe5744ce1db64a131ec2cb50059cb4bb
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2021-09-25 22:04:34 -0500

    Show characters (static information)

--- app/src/main/java/com/itiudc/breakingbadapp/fragments/CharactersListFragment.kt
+++ app/src/main/java/com/itiudc/breakingbadapp/fragments/CharactersListFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.itiudc.breakingbadapp.R
+import com.itiudc.breakingbadapp.adapters.CharacterListAdapter
+import com.itiudc.breakingbadapp.databinding.ActivityMainBinding
+import com.itiudc.breakingbadapp.databinding.FragmentCharactersListBinding
+import com.itiudc.breakingbadapp.models.Character
 
 class CharactersListFragment : Fragment() {
 
@@ -20,12 +27,21 @@ class CharactersListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_characters_list, container, false)
+        val binding = FragmentCharactersListBinding.inflate(inflater, container, false)
 
-        val title: TextView = view.findViewById(R.id.subtitleTextView)
-        title.text = resources.getStringArray(R.array.tabLayoutStrings)[0]
+        val characters = mutableListOf<Character>(
+            Character(1,"XD", "UN DIA", "https://imagenes.heraldo.es/files/image_990_v1/uploads/imagenes/2013/10/17/_walterwhite_0ae55f6d.jpg", "Alive", "CHARIBU"),
+            Character(2,"XD2", "UN DIA 2", "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2019/07/gus-fring-breaking-bad-giancarlo-esposito.jpg?itok=jlPLiQFA", "Deceased", "CHARIBU2"),
+            Character(3,"XD3", "UN DIA 3", "https://poptv.orange.es/wp-content/uploads/sites/3/2018/07/jonathan-banks-1024x698.jpg", "Presumed dead", "CHARIBU3")
+        )
+        val adapter = CharacterListAdapter(characters)
 
-        return view
+
+        binding.recyclerCharacterList.layoutManager = LinearLayoutManager(requireActivity())
+        binding.recyclerCharacterList.adapter = adapter
+
+
+        return binding.root
     }
 
 
--- app/src/main/java/com/itiudc/breakingbadapp/models/Character.kt
+++ app/src/main/java/com/itiudc/breakingbadapp/models/Character.kt
@@ -1,4 +1,12 @@
 package com.itiudc.breakingbadapp.models
 
-class Character {
+class Character(
+    val id: Int,
+    val name: String,
+    val birthday: String,
+    val img: String,
+    val status: String,
+    val nickname: String
+) {
+
 }
