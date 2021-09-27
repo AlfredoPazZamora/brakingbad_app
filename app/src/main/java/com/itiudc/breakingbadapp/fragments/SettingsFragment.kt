01f6e56d8e48fdfb58f8105aa2649a7ae6156f43
AlfredoPazZamora <46512729+AlfredoPazZamora@users.noreply.github.com>
2021-09-27 16:38:06 -0500

    Change colors, names, and desings

--- app/src/main/java/com/itiudc/breakingbadapp/fragments/SettingsFragment.kt
+++ app/src/main/java/com/itiudc/breakingbadapp/fragments/SettingsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import android.widget.TextView
 import com.itiudc.breakingbadapp.R
 
@@ -17,8 +18,9 @@ class SettingsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
 
-        val title: TextView = view.findViewById(R.id.subtitleTextView)
-        title.text = resources.getStringArray(R.array.main_tabLayout_strings)[2]
+
+
+
 
         return view
     }
